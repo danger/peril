d07f9a56dc5c19476e70313bd9cb4fdaccd8747b
Orta Therox <orta.therox@gmail.com>
2017-04-08 16:56:10 +0100

    More Logger improvements

--- source/peril.ts
+++ source/peril.ts
@@ -10,8 +10,6 @@ process.on("unhandledRejection", (reason: string, p: any) => {
 })
 
 const app = express()
-import "./setup_logger"
-
 app.set("port", process.env.PORT || 5000)
 app.set("view engine", "ejs")
 app.use(bodyParser.json())
