f32865ad1781daa986f291f937e61a35ae0ad054
Orta Therox <orta.therox@gmail.com>
2017-05-05 18:29:45 +0100

    Fix CI

--- source/github/events/_tests/_github_runner-integration.test.ts
+++
@@ -1,5 +0,0 @@
-import { readFileSync } from "fs"
-import { resolve } from "path"
-
-import fixturedPlatform from "../../../api/_tests/fixtureAPI"
-import { githubDangerRunner } from "../github_runner"
