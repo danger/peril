2535f5466bbac21faa14c6e1d3aaca85f09313bf
Orta Therox <orta.therox@gmail.com>
2017-08-12 14:09:38 -0400

    More error handling, this is great

--- source/github/events/github_runner.ts
+++ source/github/events/github_runner.ts
@@ -248,7 +248,7 @@ ${JSON.stringify(stateForErrorHandling, null, "  ")}
     const actualBranch = branch ? branch : "master"
     const message = `Could not find Dangerfile at <code>${run.dangerfilePath}</code> on <code>${repoForDangerfile}</code> on branch <code>${actualBranch}</code>`
 
-    const report = reportData("Could not find a Dangerfile at ")
+    const report = reportData(message)
     return { fails: [{ message: report }], markdowns: [], warnings: [], messages: [] }
   }
 }
