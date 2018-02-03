08c6bc7989aa49f5609b15c83648824619517957
Orta Therox <orta.therox@gmail.com>
2018-02-03 12:01:56 -0500

    Get the npm deps for the runner

--- source/runner/index.ts
+++ source/runner/index.ts
@@ -6,6 +6,8 @@ import { run } from "./run"
 
 let foundDSL = false
 
+logger.info("0.")
+
 // Wait till the end of the process to print out the results. Will
 // only post the results when the process has succeeded, leaving the
 // host process to create a message from the logs.
@@ -19,17 +21,28 @@ nodeCleanup((exitCode, signal) => {
   return undefined
 })
 
+logger.info("1.")
+
 // Start waiting on STDIN for the DSL
 getSTDIN().then(stdin => {
   foundDSL = true
   run(stdin)
 })
 
+logger.info("2.")
+
 // Add a timeout so that CI doesn't run forever if something has broken.
 setTimeout(() => {
+  logger.info("In the setTimeout")
   if (!foundDSL) {
     logger.error("Timeout: Failed to get the Peril DSL after 2 seconds")
     process.exitCode = 1
     process.exit(1)
   }
 }, 2000)
+
+logger.info("Waiting on the STDIN now.")
+import { readFileSync } from "fs"
+const stdin1 = readFileSync("/dev/stdin").toString()
+
+logger.info("Got 1:", stdin1)
