259f67f88e6916df3ea13a66cfafa96b8b55560b
Orta Therox <orta.therox@gmail.com>
2018-02-04 08:13:52 -0500

    More dumb logs

--- source/danger/append_peril.ts
+++ source/danger/append_peril.ts
@@ -1,11 +1,17 @@
+import logger from "../logger"
+logger.info("--1")
 import { PerilDSL } from "danger/distribution/dsl/DangerDSL"
-
+logger.info("--2")
 import * as NodeGithub from "@octokit/rest"
-
+logger.info("--3")
 import { DangerContext } from "danger/distribution/runner/Dangerfile"
+logger.info("--4")
 import { getTemporaryAccessTokenForInstallation } from "../api/github"
+logger.info("--5")
 import { generateTaskSchedulerForInstallation } from "../tasks/scheduleTask"
+logger.info("--6")
 import { InstallationToRun } from "./danger_runner"
+logger.info("--7")
 
 export async function appendPerilContextToDSL(installationID: number, sandbox: DangerContext, peril: PerilDSL) {
   if (sandbox.danger && sandbox.danger.github) {
@@ -36,3 +42,5 @@ export const perilObjectForInstallation = (
     Object.assign({}, ...installation.settings.env_vars.map(k => ({ [k]: environment[k] }))),
   runTask: generateTaskSchedulerForInstallation(installation.id),
 })
+
+logger.info("--8")
