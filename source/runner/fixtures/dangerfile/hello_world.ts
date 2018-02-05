83f6e87ebdbd4dfcb84aa459e770a0ae30ddb372
Orta Therox <orta.therox@gmail.com>
2018-02-04 21:27:07 -0500

    Add a hello world example

---
+++ source/runner/fixtures/dangerfile/hello_world.ts
@@ -0,0 +1,8 @@
+// tslint:disable-next-line:no-console
+
+declare const danger: any
+
+// tslint:disable-next-line:no-console
+console.log("Hello world")
+// tslint:disable-next-line:no-console
+console.log(danger.github)
