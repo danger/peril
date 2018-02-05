e25e81dd5e63f7aa3f09c817e847840cd03a4563
Orta Therox <orta.therox@gmail.com>
2018-02-04 21:23:19 -0500

    Use the runner, like we think it should

--- source/runner/run.ts
+++ source/runner/run.ts
@@ -1,11 +1,15 @@
+// I test this locally by renaming the .env file, then running:
+//
+// ❯ yarn build; cat source/runner/fixtures/branch-push.json | sed 's/12345/'"$DANGER_GITHUB_API_TOKEN"'/' | env DEBUG="*" node out/runner/index.js
+// ❯
+//
 import logger from "../logger"
 
 import { contextForDanger } from "danger/distribution/runner/Dangerfile"
 import { jsonToDSL } from "danger/distribution/runner/jsonToDSL"
 import inlineRunner from "danger/distribution/runner/runners/inline"
 
-import { getTemporaryAccessTokenForInstallation } from "../api/github"
-import { perilObjectForInstallation } from "../danger/append_peril"
+import { appendPerilContextToDSL, perilObjectForInstallation } from "../danger/append_peril"
 import { dangerRepresentationforPath, dsl } from "../danger/danger_run"
 import { executorForInstallation, InstallationToRun } from "../danger/danger_runner"
 import { getPerilPlatformForDSL } from "../danger/peril_platform"
@@ -47,15 +51,14 @@ export const run = async (stdin: string) => {
 // model, used to be much harder to keep track of their diffferences
 
 const runDangerEvent = async (installation: InstallationToRun, input: PerilRunnerObject) => {
-  const token = await getTemporaryAccessTokenForInstallation(installation.id)
-
   // const platform = getPerilPlatformForDSL(dsl.import, null, input.dsl)
   // const exec = await executorForInstallation(platform, inlineRunner)
 
   // Create a DSL that is basically just the webhook
   // TODO: This probably needs expanding to the util funcs etc
   const context = contextForDanger({ github: input.dsl } as any)
-  context.peril = perilObjectForInstallation(installation, process.env, input.peril)
+  const peril = perilObjectForInstallation(installation, process.env, input.peril)
+  await appendPerilContextToDSL(installation.id, input.token, context, peril)
 
   const dangerfileLocation = dangerRepresentationforPath(input.path)
   if (!dangerfileLocation.repoSlug) {
@@ -63,32 +66,36 @@ const runDangerEvent = async (installation: InstallationToRun, input: PerilRunne
     return
   }
 
-  const dangerfile = await getGitHubFileContentsFromLocation(token, dangerfileLocation, dangerfileLocation.repoSlug!)
+  const dangerfileContent = await getGitHubFileContentsFromLocation(
+    input.token,
+    dangerfileLocation,
+    dangerfileLocation.repoSlug!
+  )
 
   runtimeEnv = await inlineRunner.createDangerfileRuntimeEnvironment(context)
-  // if (runtimeEnv.sandbox) {
-  //   await appendPerilContextToDSL(installation.id, undefined, runtimeEnv.sandbox, peril)
-  // }
-  await inlineRunner.runDangerfileEnvironment(dangerfile, undefined, runtimeEnv)
+  await inlineRunner.runDangerfileEnvironment(dangerfileLocation.dangerfilePath, dangerfileContent, runtimeEnv)
 }
 
 const runDangerPR = async (installation: InstallationToRun, input: PerilRunnerObject) => {
-  const token = await getTemporaryAccessTokenForInstallation(installation.id)
-
   const platform = getPerilPlatformForDSL(dsl.pr, null, input.dsl)
   const exec = await executorForInstallation(platform, inlineRunner)
 
   const runtimeDSL = await jsonToDSL(input.dsl)
   const context = contextForDanger(runtimeDSL)
-  context.peril = perilObjectForInstallation(installation, process.env, input.peril)
+  const peril = perilObjectForInstallation(installation, process.env, input.peril)
+  await appendPerilContextToDSL(installation.id, input.token, context, peril)
 
   const dangerfileLocation = dangerRepresentationforPath(input.path)
 
   const defaultRepoSlug = input.dsl.github.pr.base.repo.full_name
-  const dangerfile = await getGitHubFileContentsFromLocation(token, dangerfileLocation, defaultRepoSlug)
+  const dangerfileContent = await getGitHubFileContentsFromLocation(input.token, dangerfileLocation, defaultRepoSlug)
 
   runtimeEnv = await inlineRunner.createDangerfileRuntimeEnvironment(context)
-  const results = await inlineRunner.runDangerfileEnvironment(dangerfile, undefined, runtimeEnv)
+  const results = await inlineRunner.runDangerfileEnvironment(
+    dangerfileLocation.dangerfilePath,
+    dangerfileContent,
+    runtimeEnv
+  )
   await exec.handleResultsPostingToPlatform(results)
   logger.info("Done")
 }
