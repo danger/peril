3c646b06dfe43fc24b7b439819b0c8ebd6ee4378
Orta Therox <orta.therox@gmail.com>
2018-05-25 07:08:16 -0400

    Upate staging

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -2,4 +2,4 @@ echo "Installing Now (if needed)"
 command -v now >/dev/null 2>&1 || { npm install -g now; }
 
 echo "Deploying"
-now deploy --local-config now.staging.json --team peril --token $NOW_TOKEN && now alias --local-config now.staging.json --team peril --token $NOW_TOKEN
+now deploy --local-config now.staging.json --team peril --token $NOW_TOKEN --npm && now alias --local-config now.staging.json --team peril --token $NOW_TOKEN 
