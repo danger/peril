623f566da5907b90dca36d43a55e15add0228c93
Florian PETIT <florianp37@me.com>
2018-07-07 09:19:47 +0200

    wait for the db to be set up before trying to returning installation

--- source/db/json.ts
+++ source/db/json.ts
@@ -35,6 +35,8 @@ let org: GitHubInstallation = null as any
 export const jsonDatabase = (dangerFilePath: DangerfileReferenceString): DatabaseAdaptor => ({
   /** Gets an Integration */
   getInstallation: async (_: number): Promise<GitHubInstallation | null> => {
+    const db = await getDB()
+    await db.setup()
     return org
   },
 
