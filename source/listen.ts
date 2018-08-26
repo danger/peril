1eace5e02601dc0c13ecf0523c7c8e13b9abb8f9
Orta Therox <orta.therox@gmail.com>
2018-08-25 21:28:28 -0400

    Remove apollo engine, I wasn't using it, and it would occasionally go wrong

--- source/listen.ts
+++ source/listen.ts
@@ -1,45 +1,32 @@
 import chalk from "chalk"
 import * as express from "express"
 
-import { ApolloEngine } from "apollo-engine"
 import { createServer } from "http"
 import * as Primus from "primus"
 import { setupPublicWebsocket } from "./api/api"
-import { APOLLO_ENGINE_KEY } from "./globals"
+import { PUBLIC_API_ROOT_URL } from "./globals"
 import logger from "./logger"
 
 export let primus: any = null
 
-/**
- * A function which either starts the app normally, or starts
- * it wrapped by apollo engine (for tracing and error logging)
- *
- * This is complex:
- *
- * - It creates an Apollo Engine, and a node http server
- * - The http server wraps the express app
- * - Apollo Engine then starts the listening process
- */
 export const startApp = (app: express.Express, callback: any) => {
-  if (!APOLLO_ENGINE_KEY) {
+  // Skip primus setup
+  if (!PUBLIC_API_ROOT_URL) {
     app.listen(app.get("port"), callback)
     logger.info("")
     return
   }
 
-  // https://www.apollographql.com/docs/engine/
-  const engine = new ApolloEngine({ apiKey: APOLLO_ENGINE_KEY })
   const httpServer = createServer(app as any)
 
   primus = new Primus(httpServer, { transformer: "websockets", iknowclusterwillbreakconnections: true })
   setupPublicWebsocket()
 
   // Call engine.listen instead of app.listen(port)
-  engine.listen({ port: app.get("port"), httpServer }, () => {
+  app.listen(app.get("port"), () => {
     callback()
 
     const tick = chalk.bold.greenBright("✓")
-    logger.info("  - " + tick + " Apollo Engine")
     logger.info("  - " + tick + " Primus Sockets")
   })
 }
