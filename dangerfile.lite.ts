8d651d14da7569617735f41272d968ea0b570ba3
Orta Therox <orta.therox@gmail.com>
2018-09-01 16:04:20 -0400

    dangerfile fixes

--- dangerfile.lite.ts
+++ dangerfile.lite.ts
@@ -7,7 +7,7 @@ if (!hasChangelog) {
 }
 
 // Ensure the NodeJS versions match everywhere
-const nodeVersion = JSON.parse(readFileSync("package.json", "utf8")).engines.node.substr(-1)
+const nodeVersion = JSON.parse(readFileSync("package.json", "utf8")).engines.node
 if (!readFileSync("Dockerfile", "utf8").includes("node:" + nodeVersion)) {
   warn("The `Dockerfile` does not have the same version of node in it")
 }
