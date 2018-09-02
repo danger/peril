1b19d0fec25bbb9f76115b95ebc40dd7b47a5c2c
Orta Therox <orta.therox@gmail.com>
2018-09-02 13:38:52 -0400

    Adds tests for the task/repeated schedulers

--- source/tasks/_tests/_scheduleTask-heroku.test.ts
+++ source/tasks/_tests/_scheduleTask-heroku.test.ts
@@ -1,18 +1,19 @@
-// import { generateTaskSchedulerForInstallation } from "../../tasks/scheduleTask"
+import { generateTaskSchedulerForInstallation } from "../../tasks/scheduleTask"
+import { triggerAFutureDangerRun } from "../startTaskScheduler"
 
 jest.mock("../startTaskScheduler", () => ({
   agenda: { schedule: jest.fn() },
   runDangerfileTaskName: "mockTask",
+  hasAgendaInRuntime: () => true,
+  triggerAFutureDangerRun: jest.fn(),
 }))
 
-// import { agenda } from "../startTaskScheduler"
-
 it("handles passing the task directly to agenda", () => {
-  // const scheduleFunc = generateTaskSchedulerForInstallation(123, undefined)
-  // scheduleFunc("My Task", "1 month", { hello: "world" })
-  // expect(agenda.schedule).toBeCalledWith("1 month", "mockTask", {
-  //   data: { hello: "world" },
-  //   installationID: 123,
-  //   taskName: "My Task",
-  // })
+  const scheduleFunc = generateTaskSchedulerForInstallation(123, undefined)
+  scheduleFunc("My Task", "in 1 day", { hello: "world" })
+  expect(triggerAFutureDangerRun).toBeCalledWith("1 day", {
+    data: { hello: "world" },
+    installationID: 123,
+    taskName: "My Task",
+  })
 })
