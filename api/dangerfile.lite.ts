4c66cb30f215ea7b3f55e08ca375759954946120
Orta Therox <orta.therox@gmail.com>
2019-02-13 08:03:43 -0500

    Fix local dangerfile:

--- api/dangerfile.lite.ts
+++ api/dangerfile.lite.ts
@@ -8,11 +8,8 @@ if (!hasChangelog) {
 
 // Ensure the NodeJS versions match everywhere
 const nodeVersion = JSON.parse(readFileSync("package.json", "utf8")).engines.node
-if (!readFileSync("Dockerfile", "utf8").includes("node:" + nodeVersion)) {
+if (!readFileSync("api/Dockerfile", "utf8").includes("node:" + nodeVersion)) {
   warn("The `Dockerfile` does not have the same version of node in it")
-}
-if (!readFileSync("Dockerfile.runner", "utf8").includes("node:" + nodeVersion)) {
-  warn("The `Dockerfile.runner` does not have the same version of node in it")
 }
 if (!readFileSync("../.travis.yml", "utf8").includes("node_js: " + nodeVersion)) {
   warn("The `.travis.yml` does not have the same version of node in it")
