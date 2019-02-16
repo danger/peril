c33d5607e593f1eb7bba2d76223c41c4574f9f15
Orta Therox <orta.therox@gmail.com>
2019-02-16 07:47:09 -0500

    Some fixes to tsc

--- api/source/runner/runFromExternalHost.ts
+++ api/source/runner/runFromExternalHost.ts
@@ -15,7 +15,7 @@ export const runExternally = async (
     if (call.Status === 202) {
       // Make it easy to grab logs from
       logger.info(` Logs`)
-      logger.info(`  Run a lambda job: ${call.$response.data}`)
+      logger.info(`  Run a lambda job: ${Object.keys(call.$response.data || {})}`)
     }
   } catch (error) {
     const errorMessage = `# Hyper function call failed for ${eventName}: \n\n${error}`
